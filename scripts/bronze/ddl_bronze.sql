9a9e96f1bcbd079122d316c99d1819f44ca5516f
Alireza Jazayeri <144585078+Alireza-jaz@users.noreply.github.com>
2025-08-02 17:00:56 +0330

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -40,3 +40,33 @@ CREATE TABLE raw_transaction(
     ANON NVARCHAR(255)
 );
 GO
+
+
+
+CREATE TABLE bronze.raw_transaction(
+	customer_id        NVARCHAR(250),
+	customer_name      NVARCHAR(150),
+	gender             NVARCHAR(15),
+	age                INT,
+	State              NVARCHAR(50),
+    City               NVARCHAR(50),
+    Bank_Branch        NVARCHAR(100),
+    Account_Type       NVARCHAR(50),
+    Transaction_ID     NVARCHAR(50),
+    Transaction_Date   NVARCHAR(50),
+    Transaction_Time   Time,
+    Transaction_Amount DECIMAL(18,2),
+    Merchant_ID        NVARCHAR(50),
+    Transaction_Type   NVARCHAR(50),
+    Merchant_Category  NVARCHAR(100),
+    Account_Balance    DECIMAL(18,2),
+    Transaction_Device NVARCHAR(50),
+    Transaction_Location NVARCHAR(255),
+    Device_Type        NVARCHAR(50),
+    Is_Fraud           INT,
+    Transaction_Currency NVARCHAR(10),
+    Customer_Contact   NVARCHAR(100),
+    Transaction_Description NVARCHAR(255),
+    Customer_Email     NVARCHAR(255),
+);
+GO
